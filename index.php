4b3f6d385557956ef2beb9bb6315a55123f837a4
fabestr <f.estrabaud@gmail.com>
2019-07-22 17:16:30 +0200

    boiler

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+include_once 'PHP/_debug.php';
+
+
+
+?>
+
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Crafty+Girls|Erica+One|Open+Sans|Rammetto+One&display=swap" rel="stylesheet">  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <!-- Add your site or application content here -->
+  <header>
+      <nav class = container>
+          <ul>
+              <li><a href="">Lorem.</a></li>
+              <li><a href="">Esse.</a></li>
+              <li><a href="">Vel.</a></li>
+              <li><a href="">Odit.</a></li>
+          </ul>
+      </nav>
+  </header>
+  <main>
+    <div class="container">
+        <h1>Hello world!</h1> 
+        <h3>This is HTML5 Boilerplate.</h3>
+    </div>
+  </main>
+
+  <footer>
+    <div class="container">
+    
+    </div>
+  </footer>
+
+  
+  <script src="js/vendor/modernizr-3.7.1.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set','transport','beacon'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
+</html>
